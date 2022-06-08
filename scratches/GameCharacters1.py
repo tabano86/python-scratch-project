5ccd0ab8767af786839b54888ecd5d4931f453ca
Anthony Tabano <tabano86@gmail.com>
2022-06-08 15:16:04 -0400

    sortedlist

--- scratches/GameCharacters1.py
+++ scratches/GameCharacters1.py
@@ -1,15 +1,18 @@
 # Initial list of game characters
 
-master_characters: list[str] = ["Megan Man", "Kirby", "Sonic", "Tails"]
+from sortedcontainers import SortedSet
+
+character_set = SortedSet(map(lambda x: x.upper(), ["Mega Man", "Kirby", "Sonic", "Tails"]))
 
 # Displays descending list for user
-print("There are currently {} characters on the list:".format(len(master_characters)))
-print(*master_characters, sep=", ")
+print("There are currently {} characters on the list:".format(len(character_set)))
+print(*character_set, sep=", ")
 
 # # Newer block to streamline user prompt input
 characters_to_add = input("Characters to add (separated by comma): ").split(",")
-
-master_characters = master_characters + list(map(lambda x: x.strip(), characters_to_add))
+for character in characters_to_add:
+    character_set.add(character.strip().upper())
 
 print(f"Here is your updated list with [{len(characters_to_add)}] newly added characters:")
-print(*(sorted(master_characters, reverse=True)), sep=", ")
+for c in reversed(character_set):
+    print(c)
