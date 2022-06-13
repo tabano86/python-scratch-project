ad7d145fbedaba44b74e305dd0289e255f9d8a27
Anthony Tabano <tabano86@gmail.com>
2022-06-13 17:05:38 -0400

    lambda tutorial file - part2

--- scratches/lamdas.py
+++ scratches/lamdas.py
@@ -1,4 +1,5 @@
 import functools
+import operator
 import unittest
 
 
@@ -18,6 +19,10 @@ class Tester(unittest.TestCase):
     def my_function3(input_array):
         return sum(input_array)
 
+    @staticmethod
+    def my_sum_function(a, b):
+        return a + b
+
     # Main test method
     def test(self):
         input_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -25,6 +30,10 @@ class Tester(unittest.TestCase):
         result2 = self.my_function2(input_array)
         result3 = self.my_function3(input_array)
         result4 = functools.reduce(lambda accumulator, value: accumulator + value, input_array)
+        result5 = functools.reduce(operator.add, input_array)
+        result6 = functools.reduce(self.my_sum_function, input_array)
         self.assertEqual(result1, result2)
         self.assertEqual(result2, result3)
         self.assertEqual(result3, result4)
+        self.assertEqual(result4, result5)
+        self.assertEqual(result5, result6)
